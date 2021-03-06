581b9dcd05430b3d93652951c50cf73896a6c89a
silvioprog <silvioprog@gmail.com>
2021-03-06 09:48:01 -0300

    Remove excessive spacing from the Copyright top.

--- src/Copyright.svelte
+++ src/Copyright.svelte
@@ -2,6 +2,6 @@
   export let ref: HTMLParagraphElement | undefined = undefined;
 </script>
 
-<p {...$$restProps} bind:this={ref} class="mt-3 {$$restProps.class}">
+<p {...$$restProps} bind:this={ref} class="mt-2 {$$restProps.class}">
   <small class="text-muted">Duall Sistemas LTDA &copy; 2021</small>
 </p>
