f83a6a11ebb19eef04d9b3e23189ac00e35dfa7c
silvioprog <silvioprog@gmail.com>
2021-03-07 16:31:50 -0300

    Generates the .d.ts files using svelte-dts.

--- rollup.config.js
+++ rollup.config.js
@@ -4,43 +4,44 @@ import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
+import svelteDts from 'svelte-dts';
 import pkg from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
 
-const { name } = pkg;
+export default ["es", "umd"].map((format) => {
+  const UMD = format === "umd";
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-      name
-    },
-    {
-      file: pkg.main,
-      format: 'umd',
-      sourcemap: true,
-      name
-    }
-  ],
-  plugins: [
-    svelte({
-      preprocess: sveltePreprocess({ sourceMap: !production }),
-      compilerOptions: {
-        // generate: production ? 'dom' : 'ssr',
-        hydratable: true,
-        dev: !production
+  return {
+    input: pkg.svelte,
+    output: [
+      {
+        name: UMD ? pkg.name : undefined,
+        file: UMD ? pkg.main : pkg.module,
+        format,
+        sourcemap: true
       }
-    }),
-    resolve(),
-    commonjs(),
-    typescript(),
-    production && terser()
-  ],
-  watch: {
-    clearScreen: false
-  }
-};
+    ],
+    plugins: [
+      svelte({
+        preprocess: sveltePreprocess({ sourceMap: !production }),
+        compilerOptions: {
+          // generate: production ? 'dom' : 'ssr',
+          hydratable: true,
+          dev: !production
+        }
+      }),
+      resolve(),
+      commonjs(),
+      typescript(),
+      svelteDts({
+        extensions: [".svelte", ".ts"],
+        output: pkg.types
+      }),
+      production && terser()
+    ],
+    watch: {
+      clearScreen: false
+    }
+  };
+});
