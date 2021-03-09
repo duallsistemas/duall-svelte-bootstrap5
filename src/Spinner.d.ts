072242e4e9ed1712df1d6f91509e809c598e279f
silvioprog <silvioprog@gmail.com>
2021-03-09 18:43:41 -0300

    Allow to show/hide the spinner.

--- src/Spinner.d.ts
+++ src/Spinner.d.ts
@@ -6,6 +6,7 @@ interface ISpinnerProps extends IComponentProps {
   title?: string;
   small?: boolean;
   center?: boolean;
+  visible?: boolean;
 }
 
 export default class Spinner extends SvelteComponentTyped<ISpinnerProps, { click: WindowEventMap['click'] }> {}
--- src/Spinner.svelte
+++ src/Spinner.svelte
@@ -4,17 +4,20 @@
   export let small: boolean | undefined = undefined;
   export let grow: boolean | undefined = undefined;
   export let center: boolean | undefined = undefined;
+  export let visible: boolean | undefined = true;
 </script>
 
-<div
-  bind:this={ref}
-  {...$$restProps}
-  class="spinner-{grow ? 'grow' : 'border'} {$$restProps.class}"
-  class:spinner-border-sm={small}
-  class:center
-  role="status"
-  aria-hidden="true"
-  on:click
->
-  <span class="visually-hidden">{title}</span>
-</div>
+{#if visible}
+  <div
+    bind:this={ref}
+    {...$$restProps}
+    class="spinner-{grow ? 'grow' : 'border'} {$$restProps.class}"
+    class:spinner-border-sm={small}
+    class:center
+    role="status"
+    aria-hidden="true"
+    on:click
+  >
+    <span class="visually-hidden">{title}</span>
+  </div>
+{/if}
