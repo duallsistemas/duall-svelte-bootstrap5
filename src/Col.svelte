527251745e2d63324f4747b95d40b69f31685fc5
silvioprog <silvioprog@gmail.com>
2021-03-15 17:22:03 -0300

    Add more properties to the Col and Row components.

--- src/Col.svelte
+++ src/Col.svelte
@@ -1,3 +1,7 @@
-<div class="col">
+<script lang="ts">
+  export let ref: any | undefined = undefined;
+</script>
+
+<div bind:this={ref} {...$$restProps} class="col {$$restProps.class}">
   <slot />
 </div>
--- src/Row.svelte
+++ src/Row.svelte
@@ -1,3 +1,7 @@
-<div class="row">
+<script lang="ts">
+  export let ref: any | undefined = undefined;
+</script>
+
+<div bind:this={ref} {...$$restProps} class="row {$$restProps.class}">
   <slot />
 </div>
