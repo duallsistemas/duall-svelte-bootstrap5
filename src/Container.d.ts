ae0babd66aaa6d67d598db49e273e6aa4a57535e
silvioprog <silvioprog@gmail.com>
2021-03-15 13:51:41 -0300

    Add property "fluid" to the container component.

--- src/Container.d.ts
+++ src/Container.d.ts
@@ -2,6 +2,8 @@ import { SvelteComponentTyped } from 'svelte';
 
 import { IComponentProps } from './common';
 
-interface IContainerProps extends IComponentProps {}
+interface IContainerProps extends IComponentProps {
+  fluid?: boolean;
+}
 
 export default class Container extends SvelteComponentTyped<IContainerProps, {}, { default: any }> {}
--- src/Container.svelte
+++ src/Container.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   export let ref: any | undefined = undefined;
+  export let fluid: boolean | undefined = undefined;
 </script>
 
-<div bind:this={ref} {...$$restProps} class="container {$$restProps.class}">
+<div bind:this={ref} {...$$restProps} class="container{!!fluid ? '-fluid' : ''} {$$restProps.class}">
   <slot />
 </div>
